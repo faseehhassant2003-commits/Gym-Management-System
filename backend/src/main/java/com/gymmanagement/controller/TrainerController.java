bc3a539daa46ce8875e462122bc710cc946f22f8
faseeh <faseehhassant2002@gmail.com>
2026-08-03 00:40:13 +0530

    Fix CORS for Render frontend

--- backend/src/main/java/com/gymmanagement/controller/TrainerController.java
+++ backend/src/main/java/com/gymmanagement/controller/TrainerController.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 @RestController
 
-@CrossOrigin(origins = {  "http://localhost:5173","http://localhost:5174"
+@CrossOrigin(origins = {  "http://localhost:5173", "https://gym-management-frontend-apeh.onrender.com","http://localhost:5174"
 })
 public class TrainerController {
     @Autowired
